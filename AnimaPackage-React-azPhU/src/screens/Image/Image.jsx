38b07e1aeafefc857ab4cfdd495328b656e679cb
mulkizidane <110633364+mulkizidane@users.noreply.github.com>
2024-05-28 21:50:20 +0700

    Add files via upload

---
+++ AnimaPackage-React-azPhU/src/screens/Image/Image.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const Image = () => {
+  return (
+    <div className="image">
+      <img className="exam" alt="Exam" src="/img/exam-1.png" />
+    </div>
+  );
+};
